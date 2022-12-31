3f3ff30d178b3297e0cf0aa65b0822449c43a0e6
Osama Abdullah <abdullah21673@hotmail.com>
2022-12-31 13:55:53 +0600

    server setup done

---
+++ server/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/post", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt,
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+  } catch (er) {
+    console.log(er);
+    res.status(500).send({ error: err });
+  }
+});
+
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "Osama-bot server is here",
+  });
+});
+
+app.listen(port, () => console.log(`osama bot server is running on ${port}`));
